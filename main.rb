61fe86695a5fa374b2510b7b56beb8d6cd8f1643
jacoyutorius <jacoyutorius@gmail.com>
2014-04-14 20:20:12 +0900

    upload to s3

--- main.rb
+++ main.rb
@@ -4,16 +4,59 @@ require "aws-sdk"
 require "pp"
 
 
-YAML::load(File.open("setting.yml")).each do |setting|
+# remove the object if expired.
+def delete_expired_version s3object, save_days=40
+  return if s3object.last_modified.to_date.next_day(save_days) >= Time.now.to_date
+  s3object.delete
+end
 
+
+
+
+if ARGV.length <= 0
+  raise "please select upload files..."
+end
+
+
+YAML::load(File.open("setting.yml")).each do |setting|
 
 	s3 = AWS::S3.new(
 			access_key_id: setting["AWS_ACCESS_KEY_ID"],
 			secret_access_key: setting["AWS_SECRET_ACCESS_KEY"]
 		)
 
-	s3.buckets.each do |bucket|
-		bucket.name
-	end
+
+  # buckt's version option is required
+  unless s3.buckets[setting["BUCKET_NAME"]].versioned? 
+    puts "the buckert is not version controled! please enable versioning option."
+    return 
+  end
+
+
+  s3.buckets[setting["BUCKET_NAME"]].objects.each do |obj|
+  
+    p obj.key
+    obj.versions.each do |v|
+      pp v.last_modified
+
+      delete_expired_version(v, setting["SAVE_PERIOD"])
+    end
+  end
+
+
+  # uploading
+  ARGV.each do |file|
+
+    puts file
+    raise "File does not found." unless File.exist? file
+
+    s3.buckets[setting["BUCKET_NAME"]].objects[file].write(:file => file)
+
+  end
+
 end
+
+
+
+
 
