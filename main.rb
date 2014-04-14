3cfbe13d0e5faa279211b4916785a9984c2a4d7a
yuto <jacoyutorius@gmail.com>
2014-04-14 12:07:43 +0900

    connect to s3

--- main.rb
+++ main.rb
@@ -1,11 +1,19 @@
 require "rubygems"
 require "yaml"
+require "aws-sdk"
 require "pp"
 
 
 YAML::load(File.open("setting.yml")).each do |setting|
 
-	pp setting
 
+	s3 = AWS::S3.new(
+			access_key_id: setting["AWS_ACCESS_KEY_ID"],
+			secret_access_key: setting["AWS_SECRET_ACCESS_KEY"]
+		)
+
+	s3.buckets.each do |bucket|
+		bucket.name
+	end
 end
 
