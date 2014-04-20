80c8ad4ca7316944f4d3f9cd8a9fc1ee3fdbe8ad
小木悠斗 <ogiyuto@yuto.local>
2014-04-20 09:49:44 +0900

    to behave like console app

--- application.rb
+++ application.rb
@@ -3,13 +3,14 @@ require "yaml"
 require "aws-sdk"
 require "pp"
 
-class RbackupS3::Application
+class RBackupS3
 
   def self.run argv
 
     if ARGV.length <= 0
       raise "please select upload files..."
     end
+
 
     YAML::load(File.open("setting.yml")).each do |setting|
 
@@ -28,35 +29,19 @@ class RbackupS3::Application
 
       # remove the object if expired.
       s3.buckets[setting["BUCKET_NAME"]].objects.each do |obj|
-        # p obj.key
-        obj.versions.each do |v|
-          pp v.last_modified
-          # delete_expired_version(v, setting["SAVE_PERIOD"])
 
-          next if s3object.last_modified.to_date.next_day(save_days) >= Time.now.to_date
-          puts "#{s3object.version_id} deleted"
-          s3object.delete
+        obj.versions.each do |v|
+          next if v.last_modified.to_date.next_day(setting["SAVE_PERIOD"]) >= Time.now.to_date
+          puts "#{v.version_id} deleted"
+          v.delete
         end
       end
 
 
       # uploading
       ARGV.each do |file|
-
-        # raise "File does not found.. #{file}" unless File.exist? file
-        s3.buckets[setting["BUCKET_NAME"]].objects[file].write(:file => file)
-
+        pp s3.buckets[setting["BUCKET_NAME"]].objects[file].write(:file => file)
       end
     end
   end
-
-
-  # # remove the object if expired.
-  # def delete_expired_version s3object, save_days=40
-  #   return if s3object.last_modified.to_date.next_day(save_days) >= Time.now.to_date
-    
-  #   puts "#{s3object.version_id} deleted"
-  #   s3object.delete
-  # end
-
 end
